ffcedbb5254d7113b74d5eb1476abad4419133c7
Scratchy2 <152662914+Scratchy2@users.noreply.github.com>
2024-02-15 11:21:23 -0500

    meowerclient 1.4.0

--- meowerclient.py
+++ meowerclient.py
@@ -1,23 +1,22 @@
-import requests, time, PySimpleGUI as sg, threading
+import requests, time, PySimpleGUI as sg, threading, pyautogui as p, re
 
 class MeowerClient:
     def __init__(self) -> None:
-        self.username = ""
         self.token = ""
-        self.messages = ""
-        self.history = ""
-        self.old = ""
+        self.messages = []
+        self.history = []
+        self.old = []
         self.author = ""
-        self.new = ""
+        self.new = []
         self.window = ""
+        self.p = ""
         global layout
         layout = []
 
-    def _initialize(self, username: str, token: str):
+    def _initialize(self, token: str):
         """
         Please use the RUNCLIENT() function instead of this one.
         """
-        self.username = username
         self.token = token
         self.messages = []
         self.history = requests.get("https://api.meower.org/home?autoget&page=1").json()["autoget"][0:25]
@@ -27,12 +26,19 @@ class MeowerClient:
         Please use the RUNCLIENT() function instead of this one.
         """
         for i in range(len(self.history)):
-            self.old = self.history[i]
+            self.old = self.history[24 - i]
+            self.p = self.old["p"]
+            try:
+                if self.old["unfiltered_p"]:
+                    self.p = self.old["unfiltered_p"]
+            except:
+                pass
+            
             self.author = self.old["u"]
             if self.author == "Discord":
-                self.messages.append(f"(bridged) {self.old["p"]}")
+                self.messages.append(f'(#{len(self.messages) + 1}, bridged) {self.p}')
             else:
-                self.messages.append(f"{self.author}: {self.old["p"]}")
+                self.messages.append(f'(#{len(self.messages) + 1}, meower ) {self.author}: {self.p}')
 
     def _createWin(self):
         """
@@ -41,42 +47,58 @@ class MeowerClient:
         global layout
         layout = [
             [
-                sg.Text("Meower Python Client", font=("Terminal", 32), text_color="#000"),
-		        sg.Text("", size=(0, 3))
+                sg.Text("MeowerClient", font=("Terminal", 32), text_color="#000", background_color="#e48b26"),
             ],
             [
-                sg.Text("\n".join(self.messages), font=("Terminal", 10), text_color="#000", key="history")
+                sg.Text("\n".join(self.messages), key="history", font=("Terminal", 12), text_color="#000", background_color="#e48b26"),
             ],
             [
                 sg.Input(background_color="#000", text_color="#FFF", font=("Terminal", 14), key="message", size=(100, 1)),
                 sg.Button("Send", button_color=("#FFF", "#000"), font=("Terminal", 14))
+            ],
+            [
+                sg.Button("Reply", button_color=("#FFF", "#000"), font=("Terminal", 14)),
+                sg.Button("Copy", button_color=("#FFF", "#000"), font=("Terminal", 14))
             ]
         ]
-        self.window = sg.Window(title="Meower Python Client", layout=layout, margins=(50, 100), location=(0, 0))
+        self.window = sg.Window(title="Meower Python Client", layout=layout, margins=(5, 10), location=(0, 0), background_color="#e48b26")
     
     def _run(self):
         """
         Please use the RUNCLIENT() function instead of this one.
         """
         def newmessages():
-            old = requests.get("https://api.meower.org/home?autoget&page=1").json()["autoget"][0]
-            author = old["u"]
-            if author == "Discord":
-                self.messages.append(f"(bridged) {old["p"]}")
+            self.old = requests.get("https://api.meower.org/home?autoget&page=1").json()["autoget"][0]
+            self.p = self.old["p"]
+            try:
+                if self.old["unfiltered_p"]:
+                    self.p = self.old["unfiltered_p"]
+            except:
+                pass
+            
+            self.author = self.old["u"]
+            if self.author == "Discord":
+                self.messages.append(f'(#{len(self.messages) + 1}, bridged) {self.old["p"]}')
             else:
-                self.messages.append(f"{author}: {old["p"]}")
+                self.messages.append(f'(#{len(self.messages) + 1}, meower ) {self.author}: {self.old["p"]}')
             while True:
-                new = requests.get("https://api.meower.org/home?autoget&page=1").json()["autoget"][0]
-                if new != old:
-                    old = new
-                    author = old["u"]
-                    if author == "Discord":
-                        self.messages.append(f"(bridged) {old["p"]}")
+                self.new = requests.get("https://api.meower.org/home?autoget&page=1").json()["autoget"][0]
+                if self.new != self.old:
+                    self.old = self.new
+                    self.author = self.old["u"]
+                    self.p = self.old["p"]
+                    try:
+                        if self.old["unfiltered_p"]:
+                            self.p = self.old["unfiltered_p"]
+                    except:
+                        pass
+                    
+                    if self.author == "Discord":
+                        self.messages.append(f'(#{len(self.messages) + 1}, bridged) {self.p}')
                     else:
-                        self.messages.append(f"{author}: {old["p"]}")
+                        self.messages.append(f'(#{len(self.messages) + 1}, meower ) {self.author}: {self.p}')
 
                     self.window["history"].update(value="\n".join(self.messages[len(self.messages) - 25:]))
-                    self.window["message"].update(value="")
         
                 time.sleep(0.01)
 
@@ -87,19 +109,42 @@ class MeowerClient:
             event, values = self.window.read()
             if event == "Send":
                 msg = values["message"]
-                requests.post("https://api.meower.org/home", json={"content": msg}, headers={"username": self.username, "token": self.token})
+                requests.post("https://api.meower.org/home", json={"content": msg}, headers={"username": "username", "token": self.token})
+                self.window["message"].update(value="", select=True)
+            elif event == "Reply":
+                try:
+                    idx = int(p.prompt("Message number on the list to reply to:"))
+                    pattern = "\) (.+?: )(.+)"
+                    matches = re.search(pattern, self.messages[idx - 1])
+                    if len(matches.group(2)) > 35:
+                        self.window["message"].update(value=f'@{matches.group(1)}"{matches.group(2)[:34]}..." {values["message"]}', select=True)
+                    else:
+                        self.window["message"].update(value=f'@{matches.group(1)}"{matches.group(2)}" {values["message"]}', select=True)
+                    
+                    p.hotkey("right")
+                except:
+                    print("error in input")
+            elif event == "Copy":
+                try:
+                    idx = int(p.prompt("Message number on the list to copy:"))
+                    prev = values["message"]
+                    self.window["message"].update(value=self.messages[idx - 1], select=True)
+                    p.hotkey("ctrl", "c", "backspace")
+                    self.window["message"].update(value=prev, select=True)
+                    p.hotkey("right")
+                except:
+                    print("error in input")
             elif event == sg.WIN_CLOSED:
                 exit("program stopped")
-    
+            
             time.sleep(0.01)
     
-    def RUNCLIENT(self, username: str, token: str):
+    def RUNCLIENT(self, token: str):
         """
         Function to run the client. This function will call all initialization functions for you.
         """
-        self.username = username
         self.token = token
-        MeowerClient._initialize(self=self, username=self.username, token=self.token)
+        MeowerClient._initialize(self=self, token=self.token)
         MeowerClient._showHistory(self=self)
         MeowerClient._createWin(self=self)
         MeowerClient._run(self=self)
