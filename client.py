47f097f2050e873b480e2b8034d813f8553fab60
Scratchy2 <152662914+Scratchy2@users.noreply.github.com>
2024-03-08 12:24:53 -0500

    Delete client.py from a non-folder location

--- client.py
+++
@@ -1,9 +0,0 @@
-from meowerclient import MeowerClient
-import json
-with open("token.json", "r+") as f:
-    f.seek(0)
-    data = json.load(f)
-    f.close()
-
-client = MeowerClient()
-client.RUNCLIENT(data["token"])
